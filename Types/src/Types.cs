8a78c143bb9976f3cf5282415f02dc1b8a6b01ef
kosades <kosades2014@gmail.com>
2015-02-22 20:33:38 +0300

    Types programm sources

---
+++ Types/src/Types.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Подключение библиотек
+ */
+
+using System; // Главная библиотека
+
+namespace Types.src // Пространство имен
+{
+    class Types // Класс нашей программы
+    {
+        public static void Main() // Главная функция (точка входа)
+        {
+            /*--------------------------------------------
+             *|             Основные типы данных         |
+             *-------------------------------------------- */
+
+            int     integerNumber;
+            float   floatNumber;
+            double  doubleNumber;
+            string  simpleString;
+            char    simpleChar;
+
+            
+
+            Console.ReadKey(); // Ожидание нажатия любой клавиши
+        }
+    }
+}
